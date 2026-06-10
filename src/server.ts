bacaef9c665773c44dce0ea5faf9e6ece9463684
Aiden <sophia.silva@estudantes.ifc.edu.br>
2026-06-10 15:07:19 -0300

    Refizemos o api debbuger pra ficar "mais bonito"

--- src/server.ts
+++ src/server.ts
@@ -7,7 +7,7 @@ const server = Bun.serve({
     routes: {
         "/api-debugger": Bun.file(`public/api-debugger.html`),
         "/test": {
-            GET: () => Response.json({ time: Date.now() }),
+            GET: () => Response.json({ time: Date.now() }), 
             PUT: () => Response.json({ time: Date.now() }),
             POST: () => Response.json({ time: Date.now() }),
             DELETE: () => Response.json({ time: Date.now() })
